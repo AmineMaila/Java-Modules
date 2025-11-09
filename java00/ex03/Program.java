c568bc1db460c2de56907cf7b09a46e9b4abae5b
AmineMaila <aminemaila18@gmail.com>
2025-11-09 23:46:48 +0100

    solved ex03 day00

---
+++ java00/ex03/Program.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        long storage = 0;
+        int weekCount = 0;
+        long mult = 1;
+        try (Scanner sc = new Scanner(System.in)) {
+            while (true) {
+                if (weekCount > 18) {
+                    System.err.println("IllegalArgument");
+                    System.exit(-1);
+                }
+
+                String line = sc.nextLine();
+                if (line.equals("42")) {
+                    break;
+                }
+
+                if (!line.equals("Week " + (weekCount + 1))) {
+                    System.err.println("IllegalArgument");
+                    System.exit(-1);
+                }
+
+                int min = 9;
+                for (int j = 0; j < 5; j++) {
+                    int grade = sc.nextInt();
+                    if (grade < 1 || grade > 9) {
+                        System.err.println("IllegalArgument");
+                        System.exit(-1);
+                    }
+                    if (grade < min) {
+                        min = grade;
+                    }
+                }
+                sc.nextLine();
+                storage += (long)(mult * min);
+                weekCount++;
+                mult *= 10;
+            }
+            for (int i = 0; i < weekCount; i++) {
+                System.out.print("Week " + (i + 1) + " ");
+                int lowest = (int)(storage % 10);
+                for (int j = 0; j < lowest; j++) {
+                    System.out.print('=');
+                }
+                System.out.println(">");
+                storage /= 10;
+            }
+        }
+    }
+}
