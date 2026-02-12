c4d6ca6d28cda8ed7a0d385613b593a069adf036
AmineMaila <aminemaila18@gmail.com>
2026-02-12 18:11:10 +0100

    java 06 ex00

---
+++ java06/ex00/Tests/src/main/java/fr/forty_two/numbers/NumberWorker.java
@@ -0,0 +1,7 @@
+package fr.forty_two.numbers;
+
+public class NumberWorker {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ java06/ex00/Tests/src/test/java/fr/forty_two/numbers/NumberWorkerTest.java
@@ -0,0 +1,7 @@
+package fr.forty_two.numbers;
+
+public class NumberWorkerTest {
+    public static void main(String[] args) {
+        
+    }
+}
