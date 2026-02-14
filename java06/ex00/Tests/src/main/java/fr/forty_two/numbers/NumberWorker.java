967c113a1b4b0aa2b1bbefd83c88eb6df97ce7a0
AmineMaila <aminemaila18@gmail.com>
2026-02-14 09:48:00 +0100

    java 06 ex00 finished

--- java06/ex00/Tests/src/main/java/fr/forty_two/numbers/NumberWorker.java
+++ java06/ex00/Tests/src/main/java/fr/forty_two/numbers/NumberWorker.java
@@ -1,7 +1,43 @@
 package fr.forty_two.numbers;
 
 public class NumberWorker {
-    public static void main(String[] args) {
-        
-    }
+	public class IllegalNumberException extends IllegalArgumentException {
+		
+		public IllegalNumberException(int number) {
+			super("" + number);
+		}
+	}
+
+	public boolean isPrime(int number) {
+		if (number <= 1) {
+			throw new IllegalNumberException(number);
+		}
+
+		if (number == 2) {
+			return true;
+		}
+
+		if (number % 2 == 0) {
+			return false;
+		}
+
+		double sqrt = Math.sqrt(number);
+
+		for (int i = 3; i <= sqrt; i+=2) {
+			if (number % i == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public int digitsSum(int number) {
+		long digits = number < 0 ? -(long)number : number;
+		int sum = 0;
+		while (digits > 0) {
+			sum += digits % 10;
+			digits /= 10;
+		}
+		return sum;
+	}
 }
